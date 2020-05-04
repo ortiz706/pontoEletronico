cd9447cfb71533f31ee2235972d260d2111e1d8f
ortiz <viniciusfontes@cpejr.com.br>
2020-05-04 13:57:03 -0300

    compilado do ponto teste

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ Note.get().then((note) => {
 * Configuration cron job for reports
 */
 const rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = 6;
-rule.hour = 22;
+rule.dayOfWeek = 1;
+rule.hour = 16;
 rule.minute = 00;
 // rule.tz = 'America/Sao_Paulo';
 
